ecc84b809cff0241388f315606acffd6f232bd25
Viwu <sinconsistencia@gmail.com>
2025-12-23 23:49:13 -0600

    title state completed

--- source/stokin/states/TitleState.hx
+++ source/stokin/states/TitleState.hx
@@ -5,6 +5,9 @@ import stokin.backend.codeins.CodeinBeatState;
 class TitleState extends CodeinBeatState
 {
 	public var title:FlxText;
+	public var pressAny:FlxText;
+
+	public var transitioning:Bool = false;
 
 	override public function new()
 	{
@@ -18,6 +21,30 @@ class TitleState extends CodeinBeatState
 			title.screenCenter();
 			title.y -= title.height;
 			add(title);
+		});
+
+		addCodein(CREATE, function()
+		{
+			pressAny = new FlxText();
+			pressAny.text = "Press anything to play";
+			pressAny.size = 16;
+			pressAny.screenCenter();
+			pressAny.y -= pressAny.height;
+			add(pressAny);
+		});
+
+		addCodein(UPDATE, function()
+		{
+			if (FlxG.keys.justReleased.ANY && !transitioning)
+			{
+				transitioning = true;
+
+				FlxG.camera.flash(FlxColor.WHITE, 4, function()
+				{
+					MusicBeatState.switchState(new MainMenuState());
+				});
+				FlxG.sound.play(Paths.sound('confirmMenu-wood'));
+			}
 		});
 	}
 }
